31df69fb1d9d2db7ebabae72b3ca435393cd3f19
JinKyoungmi <jkmcucu@2woo.net>
2022-08-15 16:46:48 +0900

    study02 추가
    study02 추가하고 study01 수정

--- study01.py
+++ study01.py
@@ -1,12 +1,10 @@
-a = input("숫자를 입력하세요")
+a = input("20미만의 숫자를 입력하세요")
 b = 10
 print("입력한 숫자는 : "+a)
 a = int(a)
 if a>5:
-	print("크다")
+	print("5보다 크다")
 else:
-	print("작다")
-for in i range(10):
-	if a%i == 0:
-		print(i)
-print(b)
+	print("5 이하이다")
+
+print(a+b)
